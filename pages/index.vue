1aedb92acc60eeb11ddf48e076c82834a8a4b9d0
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-09-10 10:37:47 +0200

    WIP: Transcription method

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,36 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
-export default {}
+export default {
+  mounted() {
+    this.transcribe();
+  },
+  methods: {
+    async transcribe() {
+      const config = {
+        languageCode: "en-US",
+        enableWordTimeOffsets: true,
+        model: "video",
+      };
+
+      const audio = {
+        uri: "https://res.cloudinary.com/hackit-africa/video/upload/v1631261379/nuxtjs-video-transcription/mestkweza.mp4",
+      };
+
+      const data = {
+        config,
+        audio,
+      };
+
+      const resp = await this.$axios.$post(
+        `https://speech.googleapis.com/v1/speech:recognize?key=${process.env.NUXT_ENV_GOOGLE_SPEECH_API_KEY}`,
+        data
+      );
+
+      console.log(resp);
+    },
+  },
+};
 </script>
